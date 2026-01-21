c4b580deb7fde05c0eaae2642f087eec62042197
나미 <nayoung@namiui-MacBookAir.local>
2026-01-21 20:29:47 +0900

    Chore: init spring boot project

---
+++ src/main/java/com/speakfit/backend/SpeakfitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.speakfit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakfitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakfitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speakfit/backend/SpeakfitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakfit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakfitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
