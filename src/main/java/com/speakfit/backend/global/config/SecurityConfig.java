3dcf052ee779a3ffb03b53ef371aa7711e138da6
나미 <nayoung@namiui-MacBookAir.local>
2026-01-27 02:27:45 +0900

    Feat: SecurityConfig 생성 (#1)

---
+++ src/main/java/com/speakfit/backend/global/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.speakfit.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Configuration
+    @EnableJpaAuditing
+    public class JpaConfig{}
+}
