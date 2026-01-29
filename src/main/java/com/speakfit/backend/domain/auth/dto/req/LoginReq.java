c6ff004ebe2782dac041b656501a6abdee60925c
나미 <nayoung@namiui-MacBookAir.local>
2026-01-30 05:58:02 +0900

    Refactor: RequestDTO에 스키마 어노테이션 등록 (#15)

--- src/main/java/com/speakfit/backend/domain/auth/dto/req/LoginReq.java
+++ src/main/java/com/speakfit/backend/domain/auth/dto/req/LoginReq.java
@@ -1,5 +1,6 @@
 package com.speakfit.backend.domain.auth.dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ public class LoginReq {
 
     @Getter
     @NoArgsConstructor
+    @Schema(name = "AuthLoginRequest")
     public static class Request{
         @NotBlank
         private String userId;
