34219c797b1feca641ae13d6d224dce945ab18b3
moonino <dlsgh4090@gmail.com>
2026-01-29 17:39:06 +0900

    Feat:DeleteScriptRes DTO설계 (#11)

---
+++ src/main/java/com/speakfit/backend/domain/script/dto/res/DeleteScriptRes.java
@@ -0,0 +1,12 @@
+package com.speakfit.backend.domain.script.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DeleteScriptRes {
+    private Long id;
+}
