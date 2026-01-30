a4a89d9699f63b8ea83f866fa5a76106b706b4e6
moonino <dlsgh4090@gmail.com>
2026-01-30 15:00:59 +0900

    Refactor:StopPracticeReq 형식 수정(#17)

--- src/main/java/com/speakfit/backend/domain/practice/dto/req/StopPracticeReq.java
+++ src/main/java/com/speakfit/backend/domain/practice/dto/req/StopPracticeReq.java
@@ -6,14 +6,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
-@Getter
-@Setter
-@NoArgsConstructor
+
 public class StopPracticeReq {
 
-    @NotNull(message = "연습 시간은 필수입니다.")
-    private Double time;
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Request {
 
-    // 음성 파일
-    private MultipartFile audio;
+
+        @NotNull(message = "연습 시간은 필수입니다.")
+        private Double time;
+
+        // 음성 파일
+        private MultipartFile audio;
+    }
 }
