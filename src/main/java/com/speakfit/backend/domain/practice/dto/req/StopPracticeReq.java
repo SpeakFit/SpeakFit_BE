d974afd1cd029c27926d1711767c9257691af5fb
moonino <dlsgh4090@gmail.com>
2026-01-30 17:11:54 +0900

    Refactor:StopPracticeReq dto에서 time 양수만 가능하도록 수정(#17)

--- src/main/java/com/speakfit/backend/domain/practice/dto/req/StopPracticeReq.java
+++ src/main/java/com/speakfit/backend/domain/practice/dto/req/StopPracticeReq.java
@@ -1,6 +1,7 @@
 package com.speakfit.backend.domain.practice.dto.req;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ public class StopPracticeReq {
     public static class Request {
 
 
+        @PositiveOrZero
         @NotNull(message = "연습 시간은 필수입니다.")
         private Double time;
 
