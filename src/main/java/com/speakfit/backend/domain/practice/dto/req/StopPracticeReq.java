0347b0d268cb2242c19233d01e1b2439306918e9
moonino <dlsgh4090@gmail.com>
2026-01-30 18:01:05 +0900

    Refactor:발표 영역 토큰 관련 서비스 및 컨트롤러 수정(#19)

--- src/main/java/com/speakfit/backend/domain/practice/dto/req/StopPracticeReq.java
+++ src/main/java/com/speakfit/backend/domain/practice/dto/req/StopPracticeReq.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 
 public class StopPracticeReq {
@@ -13,14 +14,16 @@ public class StopPracticeReq {
     @Getter
     @Setter
     @NoArgsConstructor
-    public static class Request {
+    @Schema(description = "발표 연습 종료 요청 객체")
+    public static class StopPracticeRequest {
 
-
+        @Schema(description = "연습 시간 (초 단위)", example = "120.5")
         @PositiveOrZero
         @NotNull(message = "연습 시간은 필수입니다.")
         private Double time;
 
         // 음성 파일
+        @Schema(description = "녹음된 오디오 파일", type = "string", format = "binary")
         private MultipartFile audio;
     }
 }
--- src/main/java/com/speakfit/backend/domain/practice/service/PracticeService.java
+++ src/main/java/com/speakfit/backend/domain/practice/service/PracticeService.java
@@ -7,9 +7,9 @@ import com.speakfit.backend.domain.practice.dto.res.StopPracticeRes;
 
 public interface PracticeService {
     // 발표 연습 시작 메소드 정의
-    StartPracticeRes startPractice(StartPracticeReq.Request request);
+    StartPracticeRes startPractice(StartPracticeReq.Request request,Long userId);
 
     // 발표 연습 종료 메소드 정의
-    StopPracticeRes stopPractice(Long practiceId, StopPracticeReq.Request request);
+    StopPracticeRes stopPractice(Long practiceId, StopPracticeReq.StopPracticeRequest request,Long userId);
 }
 
