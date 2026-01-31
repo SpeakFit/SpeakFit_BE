6cb82d8fd2db13f81dc55e54a69d6b1017d85e86
moonino <dlsgh4090@gmail.com>
2026-01-31 15:35:05 +0900

    Feat:PracticeRepository에 findAllByUserAndStatusAndCreatedAtBetween구현(#28)

--- src/main/java/com/speakfit/backend/domain/practice/repository/PracticeRepository.java
+++ src/main/java/com/speakfit/backend/domain/practice/repository/PracticeRepository.java
@@ -2,7 +2,21 @@ package com.speakfit.backend.domain.practice.repository;
 
 
 import com.speakfit.backend.domain.practice.entity.PracticeRecord;
+import com.speakfit.backend.domain.user.entity.User;
+import com.speakfit.backend.domain.practice.enums.PracticeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PracticeRepository extends JpaRepository<PracticeRecord,Long> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PracticeRepository extends JpaRepository<PracticeRecord, Long> {
+
+    // 사용자, 상태, 생성일자 범위로 연습기록조회
+    List<PracticeRecord> findAllByUserAndStatusAndCreatedAtBetween(
+            User user,
+            PracticeStatus status,
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime
+    );
+
 }
