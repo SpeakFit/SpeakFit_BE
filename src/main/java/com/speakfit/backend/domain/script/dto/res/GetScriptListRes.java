fb26d36ba4094e1e4f16931e43aa36300a022401
moonino <dlsgh4090@gmail.com>
2026-04-09 17:40:40 +0900

    Refactor:발표 대본 목록 조회 및 추가 기능 수정 (#43)

--- src/main/java/com/speakfit/backend/domain/script/dto/res/GetScriptListRes.java
+++ src/main/java/com/speakfit/backend/domain/script/dto/res/GetScriptListRes.java
@@ -1,5 +1,6 @@
 package com.speakfit.backend.domain.script.dto.res;
 
+import com.speakfit.backend.domain.script.enums.ScriptType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 public class GetScriptListRes {
     private Long id;
     private String title;
-    private String content;
+    private String content; // String (Text)
+    private ScriptType scriptType; // 추가: PPT인지 TEXT인지 구분
     private LocalDateTime createdAt;
 }
